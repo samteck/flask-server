4b5d00210ab49d8c9ab8a215a6159a3e6a7dd4eb
samarth <samarth.guptas.1995@gmail.com>
2020-03-29 07:47:42 +0100

    working server on rpi

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def hello(sam):                      # call method hello
     r = requests.get('https://corona.lmao.ninja/countries/'+sam)
     return r.text
 if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+    app.run(debug=True, port=80, host='0.0.0.0')                     # run the flask app
+   # app.run()
